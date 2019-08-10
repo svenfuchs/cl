f57f02b66e82d8acbba40581cf350e984d3936b5
Sven Fuchs <me@svenfuchs.com>
2019-08-10 20:42:13 +0200

    refactor Cast#cast

--- lib/cl/cast.rb
+++ lib/cl/cast.rb
@@ -1,33 +1,55 @@
 class Cl
   module Cast
-    TRUE  = /^(true|yes|on)$/
-    FALSE = /^(false|no|off)$/
+    class Cast < Struct.new(:type, :value, :opts)
+      TRUE  = /^(true|yes|on)$/
+      FALSE = /^(false|no|off)$/
 
-    def cast(value)
-      case type
-      when nil
-        value
-      when :array
-        Array(value).compact.flatten.map { |value| split(value) }.flatten
-      when :string, :str
-        value.to_s unless value.to_s.empty?
-      when :flag, :boolean, :bool
-        return true  if value.to_s =~ TRUE
-        return false if value.to_s =~ FALSE
-        !!value
-      when :integer, :int
-        Integer(value)
-      when :float
-        Float(value)
-      else
-        raise ArgumentError, "Unknown type: #{type}" if value
+      def apply
+        return send(type) if respond_to?(type, true)
+        raise ArgumentError, "Unknown type: #{type}"
+      rescue ::ArgumentError => e
+        raise ArgumentError.new(:wrong_type, value.inspect, type)
       end
-    rescue ::ArgumentError => e
-      raise ArgumentError.new(:wrong_type, value.inspect, type)
+
+      private
+
+        def array
+          Array(value).compact.flatten.map { |value| split(value) }.flatten
+        end
+
+        def string
+          value.to_s unless value.to_s.empty?
+        end
+        alias str string
+
+        def boolean
+          return true  if value.to_s =~ TRUE
+          return false if value.to_s =~ FALSE
+          !!value
+        end
+        alias bool boolean
+        alias flag boolean
+
+        def int
+          Integer(value)
+        end
+        alias integer int
+
+        def float
+          Float(value)
+        end
+
+        def split(value)
+          separator ? value.to_s.split(separator) : value
+        end
+
+        def separator
+          opts[:separator]
+        end
     end
 
-    def split(value)
-      separator ? value.to_s.split(separator) : value
+    def cast(value)
+      value.nil? ? value : Cast.new(type, value, separator: separator).apply
     end
   end
 end
